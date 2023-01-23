5e52db8b4f4240abaeee15622061692b3d7aad4c
Makarkarkar <ivvm3a94@gmail.com>
2023-01-23 23:01:45 +0300

    aa

---
+++ Auto.Messages/NewOwnerVehicleMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace Auto.Messages;
+
+public class NewOwnerVehicleMessage
+{
+    public string ModelCode { get; set; }
+    public int Year { get; set; }
+    public string Registration { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+
+    public NewOwnerVehicleMessage(){}
+    public NewOwnerVehicleMessage(NewOwnerMessage owner, string modelCode, int year, string registration)
+    {
+        this.ModelCode = modelCode;
+        this.Year = year;
+        this.Registration = registration;
+        this.Name = owner.Name;
+        this.Surname = owner.Surname;
+        this.Email = owner.Email;
+        this.Age = owner.Age;
+    }
+    
+}
---
+++ Auto.Notifier/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json;
+using Auto.Messages;
+using EasyNetQ;
+using Microsoft.AspNetCore.SignalR.Client;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Auto.Notifier
+{ 
+ internal class Program
+ {
+  private const string SIGNALR_HUB_URL = "https://localhost:7041/hub";
+  private static HubConnection hub;
+
+  static async Task Main(string[] args)
+  {
+   hub = new HubConnectionBuilder().WithUrl(SIGNALR_HUB_URL).Build();
+   await hub.StartAsync();
+   Console.WriteLine("Hub started!");
+   //Console.WriteLine("Press any key to send a message (Ctrl-C to quit)");
+   var amqp = "amqp://user:rabbitmq@localhost:5672";
+   using var bus = RabbitHutch.CreateBus(amqp);
+   Console.WriteLine("Connected to bus! Listening for newOwnerMessages");
+   var subscriberId = $"Auto.Notifier@{Environment.MachineName}";
+   await bus.PubSub.SubscribeAsync<NewOwnerVehicleMessage>(subscriberId, HandleNewVehicleMessage);
+   Console.ReadLine();
+  }
+
+  private static async Task HandleNewVehicleMessage(NewOwnerVehicleMessage novMessage)
+  {
+   var csvRow = $"{novMessage.ModelCode} {novMessage.Year} {novMessage.Registration} : " +
+                $"{novMessage.Name}, " +
+                $"{novMessage.Surname}, " +
+                $"{novMessage.Age}, " +
+                $"{novMessage.Email}";
+   Console.WriteLine(csvRow);
+   var json = JsonSerializer.Serialize(novMessage, JsonSettings());
+   await hub.SendAsync("NotifyWebUsers", "Auto.Notifier",
+   json);
+  }
+  
+
+  // private static async void HandleNewVehicleMessage(NewOwnerMessage now)
+  // {
+  //  var csvRow =
+  //   $"{now.Name}," +
+  //   $"{now.Surname}," +
+  //   $"{now.Email}," +
+  //   $"{now.VehicleRegistration}," +
+  //   $"{now.Age}";
+  //  Console.WriteLine(csvRow);
+  //  var json = JsonSerializer.Serialize(now, JsonSettings());
+  //  await hub.SendAsync("NotifyWebUsers", "Auto.Notifier",
+  //   json);
+  // }
+  static JsonSerializerOptions JsonSettings() =>
+   new JsonSerializerOptions
+   {
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+   };
+ }
+}
+
